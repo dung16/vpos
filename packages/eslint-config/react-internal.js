cdd9f6b42a8c2fe133872873b1952f7440642ebb
Turbobot <turbobot@vercel.com>
2024-10-08 00:38:43 +0700

    feat(create-turbo): apply official-starter transform

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,17 +1,20 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
-/*
+/**
  * This is a custom ESLint configuration for use with
  * internal (bundled by their consumer) libraries
  * that utilize React.
+ *
+ * This config extends the Vercel Engineering Style Guide.
+ * For more information, see https://github.com/vercel/style-guide
+ *
+ * @type {import("eslint").Linter.Config}
  */
-
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
+  extends: ['./base.js'],
+  plugins: ['only-warn'],
   globals: {
     React: true,
     JSX: true,
@@ -20,20 +23,12 @@ module.exports = {
     browser: true,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
-  overrides: [
-    // Force ESLint to detect .tsx files
-    { files: ["*.js?(x)", "*.ts?(x)"] },
-  ],
+  ignorePatterns: ['.*.js', 'node_modules/', 'dist/'],
+  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
 };
